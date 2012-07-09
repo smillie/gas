f998a384c3cd5f3c2e965d3e72cf74c60f8f55a0
Andrew Smillie <ajsmillie@gmail.com>
2012-07-09 16:45:35 +0100

    SSH key delete functionality added

--- sshkeys.php
+++ sshkeys.php
@@ -1,18 +1,26 @@
 <?php require 'ldapconnect.php'; ?>
+<?php
+    if (isset($_POST['delete'])) {
+        $userinfo["sshpublickey"] = str_replace("\r\n", "\n", $_POST['delete']);
+        ldap_mod_del($con, $userdn, $userinfo);
+        echo ldap_error($con);
+        header( 'Location: sshkeys.php' );
+    }
+?>
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
 
         <div class="span10">
           <div class="row-fluid">
             <div class="span4">
-              <form class="form-horizontal">
+              <form class="form-horizontal" action="sshkeys.php" method="post">
                 <fieldset>
                   <legend>SSH Keys</legend>
               
                   <?php
                     foreach (array_slice($user_get[0]["sshpublickey"], 1) as $key) {
                       echo '<div class="control-group">';
-                      echo '<label class="control-label" for="uid"><button class="btn btn-danger" href="#"><i class="icon-trash icon-white"></i> Delete</button></label>';
+                      echo '<label class="control-label" for="uid"><button name="delete" value="'.$key.'"type="submit" class="btn btn-danger" ><i class="icon-trash icon-white"></i> Delete</button></label>';
                       echo '<div class="controls">';
                       echo '<textarea class="input-xlarge" id="textarea" rows="7" disabled="disabled">'.$key.'</textarea>';
                       echo '</div>';
