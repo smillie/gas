2b132469691e70174967fa49d0695f26112cc5df
Andrew Smillie <ajsmillie@gmail.com>
2012-08-11 21:46:38 +0100

    Basic list new members page

---
+++ newusers.php
@@ -0,0 +1,83 @@
+<?php require 'ldapconnect.php'; ?>
+<?php
+if (!isUserInGroup($con, $user, "gsag")) {
+    header( 'Location: index.php' );
+}
+
+$pageTitle = ' - New Members';
+
+    
+    $mysqli = new mysqli($conf['db_host'], $conf['db_user'], $conf['db_pass'], $conf['db_name']);
+      if (mysqli_connect_errno()) {
+          printf("Connect failed: %s\n", mysqli_connect_error());
+          exit();
+      }
+      
+      /* Create the prepared statement */
+      if ($stmt = $mysqli->prepare("SELECT * FROM newusers")) {
+
+              /* Execute the prepared Statement */
+              $stmt->execute();
+
+              $stmt->bind_result($first, $last, $uid, $stuno, $email);
+              while ($stmt->fetch()) {
+                    // printf("%s %s %s %i %s\n", $first, $last, $uid, $stuno, $email);
+                    $u['first'] = $first;
+                    $u['last'] = $last;
+                    $u['uid'] = $uid;
+                    $u['stuno'] = $stuno;
+                    $u['email'] = $email;
+                    
+                    $users[] = $u;
+                }
+
+              /* Close the statement */
+              $stmt->close(); 
+      }
+      else {
+              /* Error */
+      }
+
+
+?>
+<?php require 'header.php'; ?>
+<?php require 'menu.php'; ?>
+
+    <div class="span10">
+
+            <table class="table ">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Account Name</th>
+                    <th>Full Name</th>
+                    <th>Student Number</th>
+                    <th>Email</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                <?php if (isset($error)) : ?>
+                  <div class="alert alert-error">
+                    <?php echo "$error"; ?>
+                  </div>
+                <?php elseif (isset($success)) : ?>
+                  <div class="alert alert-success">
+                    <?php echo "$success"; ?>
+                  </div>
+                <?php endif; ?>
+
+                <?php foreach ($users as $user) : ?>
+                    <tr>
+                    <td><a href="#" class="btn btn-mini">Create</a></td>
+                        <td><?php echo $user['uid']; ?></td>
+                        <td><?php echo $user['first'] . " " . $user['last']; ?></td>
+                        <td><?php echo $user['stuno']; ?></td>
+                        <td><a href="mailto:<?php echo $user['mail'][0]?>"><?php echo $user['email']; ?></a></td>
+                        <td><a href="#" class="btn btn-danger btn-mini">Delete</a></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+              </table> 
+    </div>
+<?php require 'footer.php'; ?>
