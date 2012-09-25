1574b1c76c5161b4d33178b1ec3c01e7fe428a46
Stuart Swindells <github@lumo.me.uk>
2012-09-25 16:54:49 +0100

    Tests for expiry calculation

---
+++ tests/ComputeExpiry.php
@@ -0,0 +1,36 @@
+<?php
+
+require('../functions.php');
+
+class ComputeExpiryTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this -> threshold = mktime(0, 0, 0, 5, 25, 2012);
+        $this -> expiry = mktime(0, 0, 0, 10, 5, 2012);
+        $this -> nextExpiry = mktime(0, 0, 0, 10, 4, 2013);
+    }
+    
+    public function testBeforeThreshold()
+    {
+        $date = $this -> threshold - (7 * 24 * 60 * 60);
+        $expiry = computeExpiry($date);
+        
+        $this -> assertEquals($this -> expiry, $expiry);
+    }
+
+    public function testAfterThreshold()
+    {
+        $date = $this -> threshold + (7 * 24 * 60 * 60);
+        $expiry = computeExpiry($date);
+        
+        $this -> assertEquals($this -> nextExpiry, $expiry);
+    }
+
+    public function testOnThreshold()
+    {
+        $expiry = computeExpiry($this -> threshold);
+        
+        $this -> assertEquals($this -> expiry, $expiry);
+    }
+}
